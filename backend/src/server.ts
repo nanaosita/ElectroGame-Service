b99ba8c992f84b4b81640240c1d072be827e0049
nanaosita <brendas4ntosc@gmail.com>
2026-09-17 15:22:06 -0500

    Primer commit

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const puerto = 3000;
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+res.send("Este es el back end de ElectroGame Service");
+});
+app.get("/api/informacion", (req, res) => {
+res.json({
+componente: "API REST",
+tecnologia: "Node.js, Express y TypeScript",
+estudiante1: "Brenda Naomy Santos Chay",
+matricula1: "25309088",
+estudiante2: "Itzel Nicte-Ha Miranda Manuel",
+matricula2: "2530xxxx"
+});
+});
+app.listen(puerto, () => {
+console.log(`Servidor activo en http://localhost:${puerto}`);
+});
---
+++ frontend--/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+return (
+<main>
+<h1>ElectroGame Service</h1>
+<p>Estudiante1: Brenda Naomy Santos Chay</p>
+<p>Matrícula1: 25309088</p>
+<p>Estudiante2: Itzel Nicte-Ha Miranda Manuel</p>
+<h2>Front end</h2>
+<p>Interfaz creada con React y TypeScript.</p>
+<h2>API REST</h2>
+<p>Permitirá la comunicación mediante HTTP y JSON.</p>
+<h2>Back end</h2>
+<p>Servidor preparado con Node.js y Express.</p>
+</main>
+);
+}
+export default App;
