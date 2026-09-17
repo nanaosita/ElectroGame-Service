5d81d8e816b98e37b7a995aa436bef2c7b5f53ff
nanaosita <brendas4ntosc@gmail.com>
2026-09-17 18:56:49 -0500

    version 3

--- frontend--/src/App.tsx
+++ frontend--/src/App.tsx
@@ -1,18 +1,28 @@
+import "./App.css";
+
 function App() {
-return (
-<main>
-<h1>ElectroGame Service</h1>
-<p>Estudiante1: Brenda Naomy Santos Chay</p>
-<p>Matrícula1: 25309088</p>
-<p>Estudiante2: Itzel Nicte-Ha Miranda Manuel</p>
-<p>Matrícula2: 25308154</p>
-<h2>Front end</h2>
-<p>Interfaz creada con React y TypeScript.</p>
-<h2>API REST</h2>
-<p>Permitirá la comunicación mediante HTTP y JSON.</p>
-<h2>Back end</h2>
-<p>Servidor preparado con Node.js y Express.</p>
-</main>
-);
+  return (
+    <main>
+      <h1>ElectroGame Service</h1>
+
+      <p>Servicio de reparación de equipos electrónicos</p>
+
+      <p>Estudiante1: Brenda Naomy Santos Chay</p>
+      <p>Matrícula1: 25309088</p>
+
+      <p>Estudiante2: Itzel Nicte-Ha Miranda Manuel</p>
+      <p>Matrícula2: 25308154</p>
+
+      <h2>Front end</h2>
+      <p>Interfaz creada con React y TypeScript.</p>
+
+      <h2>API REST</h2>
+      <p>Permitirá la comunicación mediante HTTP y JSON.</p>
+
+      <h2>Back end</h2>
+      <p>Servidor preparado con Node.js y Express.</p>
+    </main>
+  );
 }
+
 export default App;
